6f462e2fb94911297c74e898f732cdedcbc37179
andra <andrabaktiangjaya@gmail.com>
2024-11-25 13:03:08 +0800

    Commit Database Improve all

--- src/java/Database.java
+++ src/java/Database.java
@@ -9,29 +9,31 @@ public class Database {
     private static final String PASSWORD = "123456";
 
     public static void main(String[] args) throws SQLException {
-//        InsertProduct();
+        Product product = new Product();
+//        product.setName("abcd");
+//        product.setPrice(123);
+//        product.setCategory("hardware");
+//        product.setStatus("in stock");
+//        product.setStock(2);
+//        InsertProduct(product);
+        displayProductById(1);
+//        deleteProductById(115);
 //        getAllProduct();
 //        UpdateProduct();
-        Optional<Product> optProduct = findProductById(5);
-        if (optProduct.isPresent()) {
-            Product product = optProduct.get();
-            product.setCategory("smartphone");
-            updateProduct(product);
-            findProductById(5);
-        } else {
-            System.out.println("Product Not Found");
-        }
-        System.out.println("Connection Closed");
+//        Optional<Product> optProduct = findProductById(5);
+//        if (optProduct.isPresent()) {
+//            Product product = optProduct.get();
+//            product.setCategory("smartphone");
+//            updateProduct(product);
+//            findProductById(5);
+//        } else {
+//            System.out.println("Product Not Found");
+//        }
+//        System.out.println("Connection Closed");
 
     }
 
-    private static void InsertProduct() throws SQLException {
-        Product product = new Product();
-        product.setName("a");
-        product.setCategory("small");
-        product.setPrice(20);
-        product.setStatus("in stock");
-        product.setStock(2);
+    private static void InsertProduct(Product product) throws SQLException {
         String sql = "insert into product (name, price, category, status, stock) values (?, ?, ?, ?, ?)";
         try (var connection = DriverManager.getConnection(URL, USER, PASSWORD);) {
             PreparedStatement statement = connection.prepareStatement(sql);
@@ -102,4 +104,48 @@ public class Database {
 
         return Optional.empty();
     }
+
+    private static boolean deleteProductById(Integer id) throws SQLException {
+
+        try (var connection = DriverManager.getConnection(URL, USER, PASSWORD);) {
+            System.out.println("Connected to database");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("delete from product where id = " + id);
+            while (resultSet.next()) {
+                Product product = new Product();
+                product.setId(resultSet.getInt("id"));
+                product.setName(resultSet.getString("name"));
+                product.setPrice(resultSet.getInt("price"));
+                product.setCategory(resultSet.getString("category"));
+                product.setStatus(resultSet.getString("status"));
+                product.setStock(resultSet.getInt("stock"));
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void displayProductById(Integer id) throws SQLException {
+
+        try (var connection = DriverManager.getConnection(URL, USER, PASSWORD);) {
+            System.out.println("Connected to database");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from product where id = " + id);
+            while (resultSet.next()) {
+                Product product = new Product();
+                product.setId(resultSet.getInt("id"));
+                product.setName(resultSet.getString("name"));
+                product.setPrice(resultSet.getInt("price"));
+                product.setCategory(resultSet.getString("category"));
+                product.setStatus(resultSet.getString("status"));
+                product.setStock(resultSet.getInt("stock"));
+
+                System.out.println(String.format("Product %n Id = %d %n Name = %s %n Price = %d %n Category = %s %n Status = %s %n Stock = %d", product.getId(), product.getName(),product.getPrice(), product.getCategory(), product.getStatus(), product.getStock()));
+
+            }
+        }
+    }
+
 }
